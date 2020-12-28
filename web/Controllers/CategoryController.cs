55dbeeb096b5aa5858baf08fa600197434c4a8c6
Mustafa OGUZ <mustafa.oguzqoutlook.com.tr>
2020-12-28 20:09:57 +0300

    first commit

---
+++ web/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Kategori Listesi";
+            return View();
+        }
+
+        public IActionResult GetCategory()
+        {
+            return View();
+        }
+    }
+}
---
+++ web/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using web.Models;
+
+namespace web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Categories()
+        {
+            return View();
+        }
+    }
+}
